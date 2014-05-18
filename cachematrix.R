174eade6c39a4d7c5df20cf4bf36dc44551b4546
Steve Nelson <nelsonsteve50@gmail.com>
2014-05-18 16:41:30 -0500

    original file

---
+++ cachematrix.R
@@ -0,0 +1,19 @@
+cachematrix.R
+## included are two function makeCacheMatrix and cacheSolve.
+I am clueless about these functions.  Peer assessors, thank you but I
+don't need any points for this part of the programming assigment.
+I will need to take this course again with a better understanding of
+the amount of work entailed.
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+}
